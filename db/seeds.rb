6f4a13d68b7c54d0cbbd7d7df144572dafafec49
hasanerken <hasan.erken@gmail.com>
2017-01-05 07:43:50 +0000

    Add feed status with validation and ajax error

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create!(name:  "Hasan Erken",
              email: "he@rail.org",
-             password:              "sirena",
-             password_confirmation: "sirena",
+             password:              "sirena1",
+             password_confirmation: "sirena1",
              admin: true,
              activated: true,
              activated_at: Time.zone.now)
@@ -16,10 +16,10 @@ User.create!(name:  "Hasan Erken",
 99.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
-  password = "password"
+  password = "foobar1"
   User.create!(name:  name,
                email: email,
-               password:              password,
+               password: password,
                password_confirmation: password,
                activated: true,
                activated_at: Time.zone.now )
@@ -30,3 +30,11 @@ users = User.order(:created_at).take(6)
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
 end
+
+# Following relationships
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
